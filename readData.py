313e87bf98572961a8047d399a6db55fa30dadc2
Yang Hao <haoy12@miamioh.edu>
2024-10-22 03:43:43 -0400

    utils

---
+++ readData.py
@@ -0,0 +1,47 @@
+import os
+import torch
+import numpy as np
+from torchvision import datasets
+import torchvision.transforms as transforms
+from torch.utils.data.sampler import SubsetRandomSampler
+from utils.cutout import Cutout
+
+def read_dataset(batch_size=16, valid_size=0.2, num_workers=0, pic_path='dataset'):
+    if not os.path.exists(pic_path):
+        os.makedirs(pic_path)
+
+    transform_train = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        Cutout(n_holes=1, length=16),
+    ])
+
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
+
+    # 手动下载后设置download=False
+    train_data = datasets.CIFAR10(pic_path, train=True, download=False, transform=transform_train)
+    valid_data = datasets.CIFAR10(pic_path, train=True, download=False, transform=transform_test)
+    test_data = datasets.CIFAR10(pic_path, train=False, download=False, transform=transform_test)
+
+    num_train = len(train_data)
+    indices = list(range(num_train))
+    np.random.shuffle(indices)
+    split = int(np.floor(valid_size * num_train))
+    train_idx, valid_idx = indices[split:], indices[:split]
+
+    train_sampler = SubsetRandomSampler(train_idx)
+    valid_sampler = SubsetRandomSampler(valid_idx)
+
+    train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size,
+                                               sampler=train_sampler, num_workers=num_workers)
+    valid_loader = torch.utils.data.DataLoader(valid_data, batch_size=batch_size,
+                                               sampler=valid_sampler, num_workers=num_workers)
+    test_loader = torch.utils.data.DataLoader(test_data, batch_size=batch_size,
+                                              num_workers=num_workers)
+
+    return train_loader, valid_loader, test_loader
